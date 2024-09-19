948256ca0c576022955bfb7401293ab34141b386
kirillmihalych <tiltlessman@gmail.com>
2024-09-19 12:45:26 +0300

    setup completed

---
+++ src/LandingPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <p>Hi there</p>
+  </main>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './assets/sass/styles.scss'
+
+import { createApp } from 'vue'
+import App from './LandingPage.vue'
+
+createApp(App).mount('#app')
